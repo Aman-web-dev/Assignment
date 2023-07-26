4af9a841fd2d16bf38c5e1df2b2a1e04e1edf20b
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-07-27 00:55:53 +0530

    some Last Changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,27 @@
 
+import Link from 'next/link';
+
+import {FaPowerOff} from 'react-icons/fa'
 
 function Navbar() {
   return (
-    <div className="left-8 bg-gray-200 h-[8vh] flex align-center ">
-  <div className='flex justify-between px-4 pt-4'>
-        
-        <div className=" flex flex-row gap-4 align-center justify-center">
-                  <h2 className="font-bold ml-20 text-xl">Add New Post </h2>
-                  <h2  className="font-bold text-xl">Settings</h2>
-
-        </div>
-
-
-<div>
+    <div className=' m-auto grid lg:grid-cols-1 grid-cols-1 gap-2 p-2 w-[90vw]'>
 
 
 
-        </div>
-    </div>
-    </div>
+<div className='shrink lg:col-span-1 col-span-1 bg-white flex justify-between w-full border p-2 rounded-lg'>
+            <div className='flex align-center flex-row lg:w-full w-0 gap-5 pb-2'>
+                <p className='text-xl my-1  font-bold'>+Add New Item</p>
+
+            </div>
+            <p className='bg-blue-200  flex justify-center items-center p-2 rounded-lg'>
+               <Link className='font-bold ' href={"/"}>Logout</Link>
+                </p>
+        </div> 
+
+
+
+      </div>
   )
 }
 
